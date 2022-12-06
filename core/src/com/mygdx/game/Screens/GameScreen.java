9b122b364e9905f7bd66f4d3308746f21400d20f
ShlokM08 <108210012+ShlokM08@users.noreply.github.com>
2022-12-06 23:36:23 +0530

    c

--- core/src/com/mygdx/game/Screens/GameScreen.java
+++ core/src/com/mygdx/game/Screens/GameScreen.java
@@ -2,6 +2,7 @@ package com.mygdx.game.Screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -42,7 +43,12 @@ public class  GameScreen  implements Screen {
     private World world;
     private Box2DDebugRenderer debugRenderer;
 
-    private BodyDef bdef = new BodyDef();
+    private BodyDef bodyDefTank1 = new BodyDef();
+    private BodyDef bodyDefTank2 = new BodyDef();
+
+    private Body tank1;
+
+    private Body tank2;
 
 
 
@@ -113,10 +119,6 @@ public class  GameScreen  implements Screen {
             p2tank3.flip(true, false);
         }
 
-        //
-
-
-
 
 
         options_texture = new Texture("GameScreen_optionsbutton.png");
@@ -125,9 +127,6 @@ public class  GameScreen  implements Screen {
         options.setSize(110, 101);
         stage.addActor(options);
         stage.addActor(image);
-
-
-        //i f(TankSelection.player1Tank == TankSelection.)
 
 
 
@@ -148,88 +147,82 @@ public class  GameScreen  implements Screen {
 //GROUND
         //Body Definition
         BodyDef bodyDef = new BodyDef();
-        world = new World(new Vector2(0, -10f), true);
+        world = new World(new Vector2(0, -98.1f), true);
+        gamecam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         debugRenderer = new Box2DDebugRenderer();
         bodyDef.type = BodyDef.BodyType.StaticBody;
-        bodyDef.position.set(0,0);
-        gamecam = new OrthographicCamera(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        bodyDef.position.set(0, 0);
 
-        //fixture definition
         FixtureDef fixtureDef2 = new FixtureDef();
 
+        bodyDefTank1.type = BodyDef.BodyType.DynamicBody;
+        bodyDefTank2.type = BodyDef.BodyType.DynamicBody;
+        bodyDefTank1.position.set(-530, -105);
+        bodyDefTank2.position.set(500, -105);
+        //bdef.position.set(0,0);
 
-        bdef.type = BodyDef.BodyType.DynamicBody;
-        bdef.position.set(0,0);
         //Box
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(25, 25);
-        fixtureDef2.shape = shape;
-        fixtureDef2.density = 1.0f;
+        PolygonShape tankShape1 = new PolygonShape();
+        PolygonShape tankShape2 = new PolygonShape();
+        tankShape1.setAsBox(25, 25);
+        tankShape2.setAsBox(25, 25);
+        fixtureDef2.shape = tankShape1;
+        fixtureDef2.density = 15f;
         fixtureDef2.friction = 0.4f;
-        fixtureDef2.restitution = 0.6f;
+        fixtureDef2.restitution = 0.005f;
+
         // add box to world
-        world.createBody(bdef).createFixture(fixtureDef2);
-
-
-        //Body
-        Body body = world.createBody(bodyDef);
-        body.createFixture(shape, 0.0f);
-        shape.dispose();
+        tank1 = world.createBody(bodyDefTank1);
+        tank1.createFixture(fixtureDef2);
+        tank2 = world.createBody(bodyDefTank2);
+        tank2.createFixture(fixtureDef2);
+        //world.createBody(bodyDefTank1).createFixture(fixtureDef2);
+        System.out.println("tank1");
+        //world.createBody(bodyDefTank2).createFixture(fixtureDef2);
 
         //Ground
         ChainShape groundShape = new ChainShape();
         groundShape.createChain(new Vector2[] {
-                new Vector2(-320, -65),
-                new Vector2(-245, -65),
-                new Vector2(-178,-77),
-                new Vector2(-173,-75),
-                new Vector2(-150,-76),
-                new Vector2(-130,-75),
-                new Vector2(-95,-59),
-                new Vector2(-55,-28),
-                new Vector2(-40,-30),
-                new Vector2(-20,-28),
-                new Vector2(12,-38),
-                new Vector2(32,-55),
-                new Vector2(91,-62),
-                new Vector2(140,-33),
-                new Vector2(165,-34),
-                new Vector2(181,-43),
-                new Vector2(199,-58),
-                new Vector2(211,-62),
-                new Vector2(230,-65),
-
-
-
-                new Vector2(320,-65),
+                new Vector2(-320*2, -65*2),
+                new Vector2(-245*2, -65*2),
+                new Vector2(-178*2,-77*2),
+                new Vector2(-173*2,-75*2),
+                new Vector2(-150*2,-76*2),
+                new Vector2(-130*2,-75*2),
+                new Vector2(-95*2,-59*2),
+                new Vector2(-55*2,-28*2),
+                new Vector2(-40*2,-30*2),
+                new Vector2(-20*2,-28*2),
+                new Vector2(12*2,-38*2),
+                new Vector2(32*2,-55*2),
+                new Vector2(91*2,-62*2),
+                new Vector2(140*2,-33*2),
+                new Vector2(165*2,-34*2),
+                new Vector2(181*2,-43*2),
+                new Vector2(199*2,-58*2),
+                new Vector2(211*2,-62*2),
+                new Vector2(230*2,-65*2),
+                new Vector2(320*2,-65*2),
                // new Vector2()
         });
 
-        //fixture definition
-
-
-
-//        fixtureDef.shape = groundShape;
-//        fixtureDef.friction = .5f;
-//        fixtureDef.restitution = 0.6f;
-//        fixtureDef.density = 1;
         FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shape;
+        fixtureDef.shape = groundShape;
         fixtureDef.density = 1.0f;
         fixtureDef.friction = 0.4f;
-        fixtureDef.restitution = 0.6f;
+        fixtureDef.restitution = 0.025f;
 
         world.createBody(bodyDef).createFixture(fixtureDef);
-
-        groundShape.dispose();
     }
 
     @Override
     public void render(float delta) {
-        game.batch.setProjectionMatrix(gamecam.combined);
-
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         update(delta);
         stage.draw();
+        game.batch.setProjectionMatrix(gamecam.combined);
+
         debugRenderer.render(world, gamecam.combined);
         world.step(1/60f, 6, 2);
 
@@ -243,20 +236,38 @@ public class  GameScreen  implements Screen {
 
         batch.begin();
         if(TankSelection.player1Tank==0){
+            //add sprite to body
+            //p1tank1.setPosition(tank1.getPosition().x, tank1.getPosition().y);
             p1tank1.draw(batch);
+            /*p1tank1.setRotation((float) Math.toDegrees(tank1.getAngle()));*/
+            p1tank1.setPosition(tank1.getPosition().x+575, tank1.getPosition().y+325);
+
+
+
         }
+            //p1tank1.setPosition(tank1.getPosition().x, tank1.getPosition().y);
+            //p1tank1.draw(batch);
+            //p1tank1.setPosition(tank1.getPosition().x, tank1.getPosition().y);
+            //attach sprite to body
+            //p1tank1.setPosition();
+
         else if (TankSelection.player1Tank==1) {
-            p1tank2.draw(batch);
+            p1tank1.draw(batch);
+            p1tank1.setPosition(tank1.getPosition().x+585, tank2.getPosition().y+325);
+            //p1tank2.draw(batch);
         } else if (TankSelection.player1Tank==2) {
             p1tank3.draw(batch);
         }
 
         if(TankSelection2.player2Tank==0){
+            //attach sprite to body
             p2tank1.draw(batch);
+            p2tank1.setPosition(tank2.getPosition().x+585, tank2.getPosition().y+325);
         }
         else if (TankSelection2.player2Tank==1) {
             p2tank2.draw(batch);
         } else if (TankSelection2.player2Tank==2) {
+
             p2tank3.draw(batch);
         }
         batch.end();
@@ -387,7 +398,6 @@ public void options_menu() {
         */
 
 
-    //return null;
 
 
 
