8be0ceeaf4693de53badb2dca31e747aaa79a609
Yaksh27 <108626992+Yaksh27@users.noreply.github.com>
2022-12-18 14:55:08 +0530

    Merge branch 'main' of https://github.com/ShlokM08/AP_GAME

--- core/src/com/mygdx/game/Screens/GameScreen.java
+++ core/src/com/mygdx/game/Screens/GameScreen.java
@@ -20,6 +20,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.mygdx.game.APGAME;
+//latest code as of 18/12/2022
 public class  GameScreen  implements Screen {
     private Sprite p1tank1;
     private Sprite p1tank2;
